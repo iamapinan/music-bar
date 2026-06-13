60fb6f82518fe00e588fcf85636522c6ed77941a
Apinan Woratrakun <iamapinan@gmail.com>
2026-06-14 00:44:44 +0700

    fix: remove 3rd duplicate player from SystemChrome - truly single player
    Found PersistentYouTubePlayer + PlayerBottomBar also rendered
    in SystemChrome (wraps admin pages), creating ANOTHER YouTube
    iframe on top of the one I already removed from AdminShell.
    
    Now:
    - 1x PersistentYouTubePlayer in root layout (entire app)
    - 1x PlayerBottomBar in play page
    - 1x PlayerBottomBar in admin shell
    - SystemChrome: cleaned up, no player components

--- components/system-chrome.tsx
+++ components/system-chrome.tsx
@@ -1,18 +1,5 @@
 'use client'
 
-import { usePathname } from 'next/navigation'
-import { PersistentYouTubePlayer } from '@/components/persistent-player'
-import { PlayerBottomBar } from '@/components/player-bottom-bar'
-
 export function SystemChrome({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname()
-  const showPlayerChrome = !['/', '/apply'].includes(pathname)
-
-  return (
-    <>
-      {showPlayerChrome && <PersistentYouTubePlayer />}
-      <div>{children}</div>
-      {showPlayerChrome && <PlayerBottomBar />}
-    </>
-  )
+  return <div>{children}</div>
 }
